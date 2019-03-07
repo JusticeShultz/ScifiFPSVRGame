84d76f75ad7313d6a5dc160af4b959465154f1ee
Justice Shultz <shultzjc1@gmail.com>
2019-03-07 15:58:40 -0800

    Push pull

---
+++ ScifiVR/Assets/Scripts/SpitterAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpitterAI : MonoBehaviour {
+
+    public GameObject Player;
+    private UnityEngine.AI.NavMeshAgent Agent;
+
+    void Start()
+    {
+        UnityEngine.AI.NavMeshAgent Agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Agent.destination = Player.GetComponent<Transform>().position;
+    }
+}
