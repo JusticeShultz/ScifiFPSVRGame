a1ec0d798c479851b808eba61572c27fe336da40
Justice Shultz <shultzjc1@gmail.com>
2019-03-08 17:59:01 -0800

    Added a dynamic map and enemy to test the map with

---
+++ VRGame/Assets/Scenes/test1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+using UnityEngine.Events;
+
+public class test1 : MonoBehaviour {
+
+    public string[] names;
+    
+    
+	// Use this for initialization
+	void Start () {
+        names = Input.GetJoystickNames();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown(names[0])) { Debug.Log("worked"); }
+        
+	}
+}
