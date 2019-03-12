c726b4dd449ff51f2a90f7d1da8d744a29e22e4e
JusticeShultz <shultzjc1@gmail.com>
2019-03-11 22:59:39 -0700

    Added ammo clip art, added picking up of ammo clips, ammo clips can be stored on the belt, added a belt object with offset of your left side, added soda cans to the cantina

---
+++ VRGame/Assets/Scripts/ClipLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipLogic : MonoBehaviour
+{
+    //public Collider targetCollider;
+    public string ClipName;
+    public GameObject Gun;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        print(collision.gameObject.name);
+
+        //if (collision.collider == targetCollider)
+        if (collision.gameObject.name == ClipName)
+        {
+            ++Gun.GetComponent<Gun>().BulletClips;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ VRGame/Assets/Scripts/FollowWithOffset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWithOffset : MonoBehaviour
+{
+    public GameObject Follow;
+    public Vector3 Offset;
+    public bool FixedOffset = true;
+
+	void Update ()
+    {
+        var fixedOffset = Vector3.zero;
+
+        if (FixedOffset)
+            fixedOffset = Follow.GetComponent<Transform>().right * -0.3f;
+        GetComponent<Transform>().position = Follow.GetComponent<Transform>().position + Offset + fixedOffset;
+	}
+}
