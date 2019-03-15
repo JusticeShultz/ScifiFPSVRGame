36de3eb0d58c07308e62798d88a40bc0392d1e52
Justice Shultz <shultzjc1@gmail.com>
2019-03-15 14:37:54 -0700

    Pushing up new AI and audio changes

---
+++ VRGame/Assets/Scripts/AI_TalkTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_TalkTrigger : MonoBehaviour
+{
+    public AudioClip ToSay;
+    public GameObject ToEmit;
+    public int Delay = 4;
+    bool toggled = false;
+
+    public void Trigger()
+    {
+        if (!toggled)
+        {
+            toggled = true;
+            StartCoroutine(DoTask());
+        }
+    }
+
+    IEnumerator DoTask()
+    {
+        yield return new WaitForSeconds(Delay);
+        ToEmit.GetComponent<AudioSource>().Play();
+    }
+}
