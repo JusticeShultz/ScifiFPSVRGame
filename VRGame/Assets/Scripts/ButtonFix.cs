f472c0683727419bbd0c91c1fb49661ee8426088
Justice Shultz <shultzjc1@gmail.com>
2019-03-15 17:51:47 -0700

    Wew

--- VRGame/Assets/Scripts/ButtonFix.cs
+++ VRGame/Assets/Scripts/ButtonFix.cs
@@ -9,12 +9,15 @@ public class ButtonFix : MonoBehaviour
 	void Start ()
     {
         var watch = GameObject.Find("Watch");
+        var aiArm = GameObject.Find("Player");
+        var animation = GameObject.Find("VRCamera");
 
         HoverButton wh = GetComponent<HoverButton>();
         GetComponent<HoverButton>().onButtonDown.AddListener(delegate { GetComponent<Valve.VR.InteractionSystem.Sample.InteractibleButton>().OnButtonDown(GameObject.Find("LeftHand").GetComponent<Hand>()); });
         GetComponent<HoverButton>().onButtonDown.AddListener(delegate { GetComponent<Valve.VR.InteractionSystem.Sample.InteractibleButton>().OnButtonDown(GameObject.Find("RightHand").GetComponent<Hand>()); });
         GetComponent<HoverButton>().onButtonDown.AddListener(delegate { GetComponent<AI_TalkTrigger>().Trigger(); });
-
+        GetComponent<HoverButton>().onButtonDown.AddListener(delegate { aiArm.GetComponent<Reference>().referenceType.SetActive(true); });
+        GetComponent<HoverButton>().onButtonDown.AddListener(delegate { animation.GetComponent<Animator>().enabled = true; });
         GetComponent<AI_TalkTrigger>().ToEmit = watch;
     }
 }
