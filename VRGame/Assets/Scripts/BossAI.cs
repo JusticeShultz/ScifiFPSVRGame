af41d590d5e4575374cdff02178fc250b562270c
Thomas Foster <thomas.foster@tumwater.k12.wa.us>
2019-03-20 11:34:08 -0700

    Adding boss state machine

--- VRGame/Assets/Scripts/BossAI.cs
+++ VRGame/Assets/Scripts/BossAI.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BossAI : MonoBehaviour
 {
     public Animator animator;
+    public GameObject SmoothLook;
 
     bool IsTurning = false;
     bool IsJumping = false;
@@ -12,7 +13,16 @@ public class BossAI : MonoBehaviour
     bool IsUsingArtillary = false;
     bool IsStomping = false;
 
-    void Start () { }
+    bool PlayerInView = false;
+
+    GameObject Player;
+
+    int StunTime = 0;
+
+    void Start ()
+    {
+        Player = GameObject.Find("PlayerCollider");
+    }
 	
 	void Update ()
     {
@@ -21,5 +31,81 @@ public class BossAI : MonoBehaviour
         animator.SetBool("IsSpitting", IsSpitting);
         animator.SetBool("IsUsingArtillary", IsUsingArtillary);
         animator.SetBool("IsStomping", IsStomping);
-	}
+
+        if(StunTime > 0)
+        {
+            --StunTime;
+
+            IsTurning = false;
+            IsJumping = false;
+            IsSpitting = false;
+            IsUsingArtillary = false;
+            IsStomping = false;
+        }
+        else if(PlayerInView && Vector3.Distance(Player.transform.position, transform.position) < 7)
+        {
+            IsStomping = true;
+            IsTurning = false;
+        }
+        else
+        {
+            if (PlayerInView && Vector3.Distance(Player.transform.position, transform.position) > 7)
+            {
+                IsSpitting = true;
+                IsTurning = false;
+            }
+            else
+            {
+                if (!PlayerInView && Vector3.Distance(Player.transform.position, transform.position) > 15)
+                {
+                    IsUsingArtillary = true;
+                    IsTurning = false;
+                    StunTime = 350;
+                }
+                else
+                {
+                    if (!PlayerInView && Vector3.Distance(Player.transform.position, transform.position) < 15)
+                    {
+                        IsJumping = true;
+                        IsTurning = false;
+                        StunTime = 900;
+                    }
+                    else
+                    {
+                        if (!PlayerInView)
+                        {
+                            if (Vector3.Distance(Player.transform.position, transform.position) > 5)
+                            {
+                                IsTurning = true;
+                                SmoothLook.transform.LookAt(Player.transform.position);
+                                transform.rotation = Quaternion.Lerp(transform.rotation, SmoothLook.transform.rotation, 0.1f);
+                            }
+                            else
+                            {
+                                IsTurning = false;
+                            }
+                        }
+                        else
+                        {
+                            IsTurning = false;
+                            IsJumping = false;
+                            IsSpitting = false;
+                            IsUsingArtillary = false;
+                            IsStomping = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void EnterView()
+    {
+        PlayerInView = true;
+    }
+
+    public void ExitView()
+    {
+        PlayerInView = false;
+    }
 }
---
+++ VRGame/Assets/Scripts/BossAI_Detector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI_Detector : MonoBehaviour
+{
+    public BossAI Handler;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.name == "PlayerCollider")
+        {
+            Handler.EnterView();
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.name == "PlayerCollider")
+        {
+            Handler.ExitView();
+        }
+    }
+}
