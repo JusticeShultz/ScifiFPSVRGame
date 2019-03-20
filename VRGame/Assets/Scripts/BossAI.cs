97874b4a8aa4784b26ff2e1186e8d84cc0a091c6
Thomas Foster <thomas.foster@tumwater.k12.wa.us>
2019-03-20 10:30:40 -0700

    Animations for the boss are nearing completion, just need to add an artillary attack animation and then it's onwards to mechanics

---
+++ VRGame/Assets/Scripts/BossAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : MonoBehaviour
+{
+    public Animator animator;
+
+    bool IsTurning = false;
+    bool IsJumping = false;
+    bool IsSpitting = false;
+    bool IsUsingArtillary = false;
+    bool IsStomping = false;
+
+    void Start () { }
+	
+	void Update ()
+    {
+        animator.SetBool("IsTurning", IsTurning);
+        animator.SetBool("IsJumping", IsJumping);
+        animator.SetBool("IsSpitting", IsSpitting);
+        animator.SetBool("IsUsingArtillary", IsUsingArtillary);
+        animator.SetBool("IsStomping", IsStomping);
+	}
+}
