73c71dab0a34fa2d8e2af03bce5c9c88e0d13ce7
Justice Shultz <shultzjc1@gmail.com>
2019-04-10 17:35:24 -0700

    Reeee

--- VRGame/Assets/Scripts/FollowWithOffset.cs
+++ VRGame/Assets/Scripts/FollowWithOffset.cs
@@ -8,13 +8,33 @@ public class FollowWithOffset : MonoBehaviour
     public Vector3 Offset;
     public bool FixedOffset = true;
     public float OffsetAmount = -0.3f;
+    public bool IsClip = false;
+    public GameObject Head;
 
-	void Update ()
+    void Update ()
     {
         var fixedOffset = Vector3.zero;
 
-        if (FixedOffset)
+        if (!IsClip)
+        {
+            if (FixedOffset)
+                fixedOffset = Follow.GetComponent<Transform>().right * OffsetAmount;
+            GetComponent<Transform>().position = Follow.GetComponent<Transform>().position + Offset + fixedOffset;
+        }
+        else
+        {
+            RaycastHit hit;
+            Ray ray = new Ray();
+            ray.origin = Follow.transform.position;
+            ray.direction = -Vector3.up;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //Offset = (Follow.transform.position + Head.transform.position) / 2;
+            }
+                //Offset = 
             fixedOffset = Follow.GetComponent<Transform>().right * OffsetAmount;
-        GetComponent<Transform>().position = Follow.GetComponent<Transform>().position + Offset + fixedOffset;
+            GetComponent<Transform>().position = Follow.GetComponent<Transform>().position + Offset + fixedOffset;
+        }
 	}
 }
